45dbcc452a01cd054ccd977dd4a8b993393a9548
Filip Bártek <filip.bartek@hotmail.com>
2014-01-02 19:14:27 +0100

    Reformats data; determines correct hits

--- analyze.R
+++ analyze.R
@@ -56,10 +56,61 @@ dataAll = reshape::merge_all(dataList)
 
 dataAll = subset(dataAll, select = -c(X)) # empty column added by PsychoPy
 
+# block
 dataAll$block <- NA
 dataAll <- transform(dataAll, block = ifelse(!is.na(trials.thisRepN), "trials", block))
 dataAll <- transform(dataAll, block = ifelse(!is.na(block0.thisRepN), "block0", block))
 dataAll <- transform(dataAll, block = ifelse(!is.na(block1.thisRepN), "block1", block))
 dataAll$block <- factor(dataAll$block, levels = c("trials", "block0", "block1"), ordered = TRUE)
 
-#dataBlocks <- subset(dataAll, block %in% c("block0", "block1"))
+# thisRepN
+dataAll <- transform(dataAll,
+	thisRepN = ifelse(!is.na(trials.thisRepN), trials.thisRepN,
+		ifelse(!is.na(block0.thisRepN), block0.thisRepN,
+			ifelse(!is.na(block1.thisRepN), block1.thisRepN, NA)
+		)
+	)
+)
+dataAll <- subset(dataAll, select = -c(trials.thisRepN, block0.thisRepN, block1.thisRepN))
+
+# thisTrialN
+dataAll <- transform(dataAll,
+	thisTrialN = ifelse(!is.na(trials.thisTrialN), trials.thisTrialN,
+		ifelse(!is.na(block0.thisTrialN), block0.thisTrialN,
+			ifelse(!is.na(block1.thisTrialN), block1.thisTrialN, NA)
+		)
+	)
+)
+dataAll <- subset(dataAll, select = -c(trials.thisTrialN, block0.thisTrialN, block1.thisTrialN))
+
+# thisN
+dataAll <- transform(dataAll,
+	thisN = ifelse(!is.na(trials.thisN), trials.thisN,
+		ifelse(!is.na(block0.thisN), block0.thisN,
+			ifelse(!is.na(block1.thisN), block1.thisN, NA)
+		)
+	)
+)
+dataAll <- subset(dataAll, select = -c(trials.thisN, block0.thisN, block1.thisN))
+
+# thisIndex
+dataAll <- transform(dataAll,
+	thisIndex = ifelse(!is.na(trials.thisIndex), trials.thisIndex,
+		ifelse(!is.na(block0.thisIndex), block0.thisIndex,
+			ifelse(!is.na(block1.thisIndex), block1.thisIndex, NA)
+		)
+	)
+)
+dataAll <- subset(dataAll, select = -c(trials.thisIndex, block0.thisIndex, block1.thisIndex))
+
+dataAll <- subset(dataAll, select = c(participantId, block, target_side, target_time, cue_side, trialKey.rt))
+
+dataBlocks <- subset(dataAll, block %in% c("block0", "block1"))
+dataBlocks <- subset(dataAll, select = -c(block))
+
+dataRes <- dataBlocks
+
+dataRes <- transform(dataRes, exp.rt = (target_time / 1000) + 0.5)
+dataRes <- transform(dataRes, delay = trialKey.rt - exp.rt)
+dataRes <- transform(dataRes, corrDelay = (delay >= 0 & delay <= 0.5))
+dataRes <- transform(dataRes, corr = (target_side == "none" & is.na(trialKey.rt)) | (target_side != "none" & !is.na(corrDelay) & corrDelay))
